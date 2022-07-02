1c5be9ab24decb3d4db518b0600c1b67d4243597
ignaciano3 <igarciap@fi.uba.ar>
2022-07-02 13:47:19 -0300

    .

--- Grafo.py
+++ Grafo.py
@@ -22,29 +22,3 @@ class Grafo:
             ady.append(i)
         ady.sort()
         return ady
-
-class UnionFind:
-    def __init__(self, largo):
-        self.uf = [i for i in range(largo)]
-        self.rank = [0 for _ in range(largo)]
-    
-    def find(self, v):
-        if self.uf[v] == v:
-            return v
-        else:
-            return self.find(self.uf[v])
-    
-    def union(self, v1, v2):
-        v1root = self.find(v1)
-        v2root = self.find(v2)
-
-        if self.rank[v1root] < self.rank[v2root]:
-            self.uf[v1root] = v2root
-        elif self.rank[v1root] > self.rank[v2root]:
-            self.uf[v2root] = v1root
-        else:
-            self.uf[v2root] = v1root
-            self.rank[v1root] +=1
-    
-    def print_uf(self):
-        print([chr(i+65) for i in self.uf])
